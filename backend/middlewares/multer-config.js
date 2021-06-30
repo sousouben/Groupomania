c5ca0a6e37338bbbf62c4c99990cdd607f16b93a
sousouben <s.mouatakide@laposte.net>
2021-06-30 15:03:13 +0200

    creation multer config

---
+++ backend/middlewares/multer-config.js
@@ -0,0 +1,22 @@
+const multer = require('multer');
+
+const MIME_TYPES = {
+    'image/jpeg': 'jpg',
+    'image/jpg': 'jpg',
+    'image/png': 'png',
+    'image.gif': 'gif',
+    'image.webp': 'webp'
+};
+
+const storage = multer.diskStorage({
+    destination: (req,file,callback)=>{
+        callback(null,'images');
+    },
+    filename:(req,file,callback)=>{
+        const name = file.originalname.replace(/\.[^/.]+$/, "");
+        const extension = MIME_TYPES[file.mimetype];
+        callback(null,name + Date.now() + "." + extension);
+  },
+});
+
+module.exports = multer({storage: storage}).single('image');
