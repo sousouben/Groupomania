5ec3b3f11b836e8d212b65c6790df8035c62b387
sousouben <s.mouatakide@laposte.net>
2021-07-08 10:35:38 +0200

    controller post et comments debut

--- backend/controllers/comments.js
+++ backend/controllers/comments.js
@@ -3,6 +3,17 @@ const fs = require('fs');
 
 exports.createComment = async(req,res)=>{
     try{
+        let comments = req.body.comments;
+        const newCom = await models.Comment.create({
+            comments: comments,
+            userId: req.user.id,
+            postId: req.params.id
+        });
+        if (newCom){
+            res.status(201).json({ message: 'Votre commentaire a été envoyé',newCom});
+        }else{
+            throw new Error('Désolé, quelque chose s\'est mal passé');
+        }
 
     }catch(error){
         res.status(400).json({
@@ -13,6 +24,25 @@ exports.createComment = async(req,res)=>{
 
 exports.getComments = async(req,res)=>{
     try{
+        const order = req.query.order;
+        const comments = await models.Comment.findAll({
+            attributes:[
+                "id",
+                "userId",
+                "contents",
+                "status",
+                "createdAt",
+                "updatedAt"
+            ],
+            order: [order != null ? order.split(':') : ['createdAt', 'DESC']],
+            where: { postId: req.params.id},
+            include: [{ model: models.User,attributes: ['pseudo']}]
+        }),
+        if(comments){
+            res.status(200).send({ message:comments });
+        }else{
+            throw new Error('Il n\'y a pas de commentaire');
+        }
 
     }catch(error){
         res.status(400).json({
--- backend/controllers/posts.js
+++ backend/controllers/posts.js
@@ -5,6 +5,32 @@ const fs = require('fs');
 //Permet de créer un nouveau post
 exports.createPost = async(req, res)=>{
     try{
+        //image
+        const imageUrl = `${req.protocol}://${req.get('host')}/images/${req.file.filename}`;
+        if(!imageUrl){
+            throw new Error('Désolé, ils y a des paramètres manquants');
+        }
+        
+        //user
+        const findUser = await models.User.findUser({
+            attributes: ['pseudo', 'email'],
+            where: { id:req.user.id },
+        });
+        if(!findUser){
+            throw new Error('Désolé, nous ne trouvons pas votre compte');
+        }
+
+        //post
+        const newPost = await models.Post.create({
+            title: req.body.title,
+            content: req.body.content,
+            image: imageUrl,
+            userId: req.user.id
+        });
+        if(!newPost){
+            throw new Error('Désolé, paramètre manquants');
+        }
+        res.status(200).json({ error: error.message });
 
     }catch(error){
         res.status(400).json({
@@ -16,6 +42,24 @@ exports.createPost = async(req, res)=>{
 //Récupérer la liste de tous les posts
 exports.getAllPosts = async(req, res)=>{
     try{
+        const fields = req.query.fields; //champs
+        const order = req.query.order;
+
+        const posts = await models.Post.findAll({
+            order: [ order !=null ? order.split(':') : ['createdAt', 'DESC']],
+            attributes: fields != '*' && fields !=null ? fields.split(',') : null,
+            include: [
+                {
+                    model : models.User,
+                    attributes: ['pseudo', 'isAdmin'],
+                },
+            ],
+        });
+
+        if(!posts){
+            throw new Error('Désolé, sa na rien récupérer');
+        }
+        res.status(200).send(posts);
 
     }catch(error){
         res.status(400).json({
@@ -27,6 +71,7 @@ exports.getAllPosts = async(req, res)=>{
 //Récupérer un post
 exports.getPostProfile = async(req, res)=>{
     try{
+
 
     }catch(error){
         res.status(400).json({
